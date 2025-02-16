f91861e8766bdd685ce3b7d067a6b3da0f6feb14
Aaron Nazareth <aaron@louisa.ai>
2025-02-16 16:45:25 +0530

    feat: backend changes

--- backend/src/api/jobs.py
+++ backend/src/api/jobs.py
@@ -1,8 +1,23 @@
 from jobspy import scrape_jobs
 import pandas as pd
-from backend.models.job import Job
+from src.logger import logger
 
-def get_jobs(city: str, country_code: str, country, job_title: str, recruiters: list = []):
+indeed_countries = [
+    "argentina", "australia", "austria", "bahrain", "belgium", "brazil", "canada", "chile", "china", "colombia", "costa rica", "czech republic", "czechia", "denmark", "ecuador", "egypt", "finland", "france", "germany", "greece", "hong kong", "hungary", "india", "indonesia", "ireland", "israel", "italy", "japan", "kuwait", "luxembourg", "malaysia", "malta", "mexico", "morocco", "netherlands", "new zealand", "nigeria", "norway", "oman", "pakistan", "panama", "peru", "philippines", "poland", "portugal", "qatar", "romania", "saudi arabia", "singapore", "south africa", "south korea", "spain", "sweden", "switzerland", "taiwan", "thailand", "türkiye", "turkey", "ukraine", "united arab emirates", "uk", "united kingdom", "usa", "us", "united states", "uruguay", "venezuela", "vietnam", "usa/ca", "worldwide"
+]
+
+def validate_indeed_country(country: str):
+    if country.lower() not in indeed_countries:
+        for indeed_country in indeed_countries:
+            if country.lower() in indeed_country:
+                return indeed_country
+            if indeed_country in country.lower():
+                return indeed_country
+        return "worldwide"
+    return country
+    
+
+def get_jobs(city: str, country_code: str, country: str, job_title: str, recruiters: list = []):
     supported_recruiters = ["indeed", "linkedin", "zip_recruiter", "glassdoor", "google"]
     if len(recruiters) > 0:
         for recruiter in recruiters:
@@ -10,6 +25,7 @@ def get_jobs(city: str, country_code: str, country, job_title: str, recruiters: 
                 return f"Recruiter {recruiter} is not supported. Supported recruiters are: {supported_recruiters}"
     else:
         recruiters = supported_recruiters
+    country = validate_indeed_country(country)
     jobs = scrape_jobs(
         site_name=["indeed", "linkedin", "zip_recruiter", "glassdoor", "google"],
         search_term=job_title,
@@ -32,7 +48,9 @@ def write_to_csv(jobs: pd.DataFrame, file_name: str):
 def print_jobs_table(jobs: pd.DataFrame):
     print(jobs)
 
-def get_jobs_api_response(city: str, country_code: str, country, job_title: str, recruiters: list = []) -> list[Job]:
+def get_jobs_api_response(city: str, country_code: str, country, job_title: str, recruiters: list = []) -> list[dict]:
+    logger.info("Getting jobs...")
+    logger.info(f"{city} {country_code} {country} {job_title} {recruiters}")
     jobs = get_jobs(city, country_code, country, job_title, recruiters)
     jobs = jobs.fillna("")
     return jobs.to_dict(orient="records")
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,37 @@
-from fastapi import FastAPI
-from backend.models.job import Job
-from backend.src.api.jobs import get_jobs_api_response
+from datetime import date, datetime
+import json
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import PlainTextResponse, JSONResponse
+from models.job import Job
+from src.api.jobs import get_jobs_api_response
+from src.logger import logger
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (date, datetime)):
+            return obj.isoformat()
+        return super().default(obj)
+
+def serialize_dates(obj):
+    if isinstance(obj, (date, datetime)):
+        return obj.isoformat()
+    elif isinstance(obj, dict):
+        return {k: serialize_dates(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [serialize_dates(i) for i in obj]
+    else:
+        return obj
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
@@ -11,18 +39,26 @@ def read_root():
 
 
 @app.get("/jobs")
-def get_jobs(city: str, country_code: str, country: str, job_title: str, recruiters: str) -> list[Job]:
+def get_jobs(city: str, country_code: str, country: str, job_title: str, recruiters: str) -> list:
     # some validation
     if not city:
-        return {"error": "city is required"}
+        return HTTPException(status_code=400, detail="city is required")
     if not country_code:
-        return {"error": "country_code is required"}
+        return HTTPException(status_code=400, detail="country_code is required")
     if not country:
-        return {"error": "country is required"}
+        return HTTPException(status_code=400, detail="country is required")
     if not job_title:
-        return {"error": "job_title is required"}
+        return HTTPException(status_code=400, detail="job_title is required")
     if not recruiters:
-        return {"error": "recruiters is required"}
-    recruiters = recruiters.split(",")
-    return get_jobs_api_response(city, country_code, country, job_title, recruiters)
+        recruiters = ""
+    recruiters_list = recruiters.split(",")
+    recruiters_list.remove("")
+    jobs = get_jobs_api_response(city, country_code, country, job_title, recruiters_list)
+    logger.info(f"Returning {len(jobs)} jobs")
+    serialized_jobs = serialize_dates(jobs)
+    return JSONResponse(
+        content=serialized_jobs,
+        media_type="application/json",
+    )
+    
 
