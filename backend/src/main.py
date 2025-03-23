3bf8999f0ff90ebeb2d7f4ec372e29f16e9a2065
Aaron Nazareth <aaron@louisa.ai>
2025-03-23 19:20:55 +0530

    allow any origin

--- backend/src/main.py
+++ backend/src/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[frontend_url],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
